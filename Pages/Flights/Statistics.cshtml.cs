f690c0eaf2a02e4040cc55531b3cd201ad80d921
Deepika Srivastava <srivasdv@mail.uc.edu>
2024-10-11 04:36:32 -0400

    Stat_changes
    Stat_changes

--- Pages/Flights/Statistics.cshtml.cs
+++ Pages/Flights/Statistics.cshtml.cs
@@ -23,19 +23,30 @@ namespace FinalProject.Pages.Flights
 
         public int TotalFlights { get; set; }
         public int TotalUsers { get; set; }
+        public int TotalBookings { get; set; }
+        public int TotalAirports { get; set; }
+        public int TotalCities  { get; set; }
+        public int TotalLounges { get; set; }
+        public int TotalSeats { get; set; }
+
+
 
         public async Task OnGetAsync()
         {
             try
             {
                 TotalFlights = await _context.Flight.CountAsync();
-                TotalUsers = await _context.Users.CountAsync();
+                TotalUsers = await _context.User.CountAsync();
+                TotalBookings = await _context.Booking.CountAsync();
+                TotalAirports = await _context.Airport.CountAsync();
+                TotalCities = await _context.City.CountAsync();
+                TotalLounges = await _context.Lounge.CountAsync();
+                TotalSeats = await _context.Seat.CountAsync();
             }
             catch (Exception ex)
             {
-                // Log the exception
                 ModelState.AddModelError(string.Empty, "An error occurred while retrieving flight statistics.");
-                Console.WriteLine(ex.Message); // For debugging purposes
+                Console.WriteLine(ex.Message);
             }
         }
     }
